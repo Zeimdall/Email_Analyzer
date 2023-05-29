75086e341ce2e0f4058a497efaec6307683cff15
Jan Banot <banot.jan@gmail.com>
2023-05-29 22:40:40 +0200

    tmp_changes

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from email_analyzer_gui import EmailAnalyzerGui
+from gui import gui
+from logic import logic
 
 if __name__ == "__main__":
-    gui = EmailAnalyzerGui()
+    # gui = EmailAnalyzerGui()
+    gui = gui()
+    logic = logic(gui)
