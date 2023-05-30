b2f361cf4d3fe3a3bfd170b812a1c28c8baae930
Zeimdall <48431176+Zeimdall@users.noreply.github.com>
2023-05-30 20:57:55 +0200

    Tmp branch (#1)
    * tmp_changes
    
    * fix
    
    * fix imports
    
    * modify files
    
    ---------
    
    Co-authored-by: Jan Banot <banot.jan@gmail.com>

--- utils.py
+++ utils.py
@@ -1,8 +1,8 @@
 import os
 from tkinter import messagebox
 import shutil
-
 import ipapi
+import requests
 
 
 def exported_path():
@@ -27,13 +27,35 @@ def show_error(message):
     messagebox.showerror("Error", message)
 
 
-def check_ip_reputation(ip_address):
+def check_ip(ip_address):
+    url = f"https://api.threatintelligenceplatform.com/v1/ip/{ip_address}"
     try:
-        ip_data = ipapi.location(ip_address, output='json')
-        if 'threat' in ip_data:
-            threat_level = ip_data['threat']['is_threat']
-            if threat_level:
-                return "Unsafe IP address"
+        response = requests.get(url)
+        if response.status_code == 200:
+            ip_data = response.json()
+            if 'is_threat' in ip_data:
+                threat_level = ip_data['is_threat']
+                if threat_level:
+                    return "Unsafe IP address"
+            return "Safe IP address"
+        else:
+            return "IP information not available"
+    except requests.exceptions.RequestException:
+        return "Error occurred during IP reputation check"
+
+
+UNSAFE_IPS = [
+    '2.56.119.93', '24.165.207.194', '45.61.185.249', '50.203.7.250', '162.247.74.216',
+    '78.142.18.219', '107.189.4.83', '141.98.11.195', '146.71.0.48', '185.243.218.46'
+]
+
+
+def check_ip_reputation(ip_address):
+
+    if ip_address in UNSAFE_IPS:
+        return "Unsafe IP address"
+    else:
         return "Safe IP address"
-    except ipapi.exceptions.PageNotFound:
-        return "IP information not available"
+
+
+
